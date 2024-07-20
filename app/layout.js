afb581a837c60ac1727f75c90e3f80d34682c44e
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-07-21 01:49:24 +0500

    Edited styling

--- app/layout.js
+++ app/layout.js
@@ -13,20 +13,20 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-white">
+      <body className="bg-white relative">
         <Header />
         {children}
         <Footer />
         <a
-          href="https://wa.me/03204016684" // Replace with your WhatsApp number
-          className="whatsapp_float"
+          href="https://wa.me/YOUR_PHONE_NUMBER" // Replace with your WhatsApp number
+          className="fixed bottom-10 right-2  shadow-green-400  bg-green-500 text-white rounded-full flex items-center justify-center shadow-lg z-50 hover:bg-green-600 transition duration-300"
           target="_blank"
           rel="noopener noreferrer"
         >
           <img
             src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/WhatsApp_icon.png/479px-WhatsApp_icon.png" // Ensure this image is in your public directory
             alt="WhatsApp"
-            className="whatsapp_icon "
+            className="w-16 sm:w-12 p-1 "
           />
         </a>
       </body>
