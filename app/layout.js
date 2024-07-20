b215967db1b1b087a03569d893e8742f7d40a578
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-07-20 17:37:18 +0500

    disabled footer

--- app/layout.js
+++ app/layout.js
@@ -16,7 +16,7 @@ export default function RootLayout({ children }) {
       <body className="bg-white ">
         <Header />
         {children}
-        <Footer />
+        {/* <Footer /> */}
       </body>
     </html>
   )
