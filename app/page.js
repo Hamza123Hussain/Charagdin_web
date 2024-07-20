d831c3dbf183fd86fa95e6c819e1acfab8594f7c
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-07-20 17:37:46 +0500

    Best Sellers Category added

--- app/page.js
+++ app/page.js
@@ -1,3 +1,4 @@
+import BestSellers from '@/components/BestSellers'
 import Carousel from '@/components/Carosuel'
 import Image from 'next/image'
 
@@ -5,6 +6,7 @@ export default function Home() {
   return (
     <>
       <Carousel />
+      <BestSellers />
     </>
   )
 }
