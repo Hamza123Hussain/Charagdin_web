5cb9e6d2fd50669d2e92215f9a99695a2895456d
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-07-20 18:52:44 +0500

    added shadow on hover

--- components/Category.jsx
+++ components/Category.jsx
@@ -2,7 +2,8 @@ import Image from 'next/image'
 import SOFA from '../public/SOFA.jpg'
 export default function CategoryCard() {
   return (
-    <div className="w-fit rounded-lg flex flex-col justify-center items-center  bg-white border border-gray-200  hover:shadow-slate-500  shadow-md dark:bg-gray-800 dark:border-gray-700">
+    //dark:bg-gray-800 dark:border-gray-700
+    <div className="w-fit rounded-lg flex flex-col justify-center items-center  bg-white border border-gray-200  hover:shadow-slate-500  shadow-md ">
       <a href="#">
         <Image className="rounded-t-lg" src={SOFA} width={400} height={225} />
       </a>
