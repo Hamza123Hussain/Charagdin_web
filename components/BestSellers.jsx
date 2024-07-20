4fc8a60fafecc521b298120fa805c41bbf90e767
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-07-20 18:17:51 +0500

    adjusted margins

--- components/BestSellers.jsx
+++ components/BestSellers.jsx
@@ -3,11 +3,11 @@ import BlogCard from './ProductCard'
 
 const BestSellers = () => {
   return (
-    <div className=" p-2">
-      <h1 className=" font-extrabold text-4xl text-center sm:text-left">
+    <div className=" p-2 mt-5">
+      <h1 className=" font-extrabold sm:px-5 text-2xl sm:text-4xl text-center sm:text-left">
         Best Sellers
       </h1>
-      <div className="p-5 grid grid-cols-1 sm:grid-cols-4 mx-auto justify-center items-center gap-5">
+      <div className="p-5 grid grid-cols-2 sm:grid-cols-4 mx-auto justify-center items-center gap-5">
         <BlogCard />
         <BlogCard />
         <BlogCard />
