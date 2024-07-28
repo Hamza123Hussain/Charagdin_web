97be647448cb973581c9aaa88d0735837e847888
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-07-28 20:39:08 +0500

    Routing from category to products

--- components/Category.jsx
+++ components/Category.jsx
@@ -1,12 +1,20 @@
+'use client'
 import Image from 'next/image'
 import SOFA from '../public/SOFA.jpg'
+import { useRouter } from 'next/navigation'
 export default function CategoryCard() {
+  const Router = useRouter()
   return (
     //dark:bg-gray-800 dark:border-gray-700
     <div className="w-full rounded-lg flex flex-col justify-center items-center  bg-white border border-gray-200  hover:shadow-slate-500  shadow-md ">
-      <a href="#">
-        <Image className="rounded-t-lg" src={SOFA} width={400} height={225} />
-      </a>
+      <Image
+        onClick={() => Router.push('/Products/sofa')}
+        className="rounded-t-lg"
+        src={SOFA}
+        width={400}
+        height={225}
+      />
+
       <div className=" p-1">
         <h3 className="text-2xl uppercase font-bold tracking-tight text-slate-700 dark:text-white">
           Sofa
