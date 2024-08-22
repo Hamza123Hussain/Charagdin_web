e5f3737923c7196d78ab0c94d0ceea371fa914c9
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-22 21:39:36 +0500

    change the style and removed unused data

--- app/Products/[Name]/page.jsx
+++ app/Products/[Name]/page.jsx
@@ -4,10 +4,10 @@ import * as React from 'react'
 
 export default function MediaCard({ params }) {
   return (
-    <div className=" p-10">
+    <div className=" p-10 bg-white">
       {' '}
-      <h1 className=" text-center text-6xl"> Sofa are here</h1>
-      <div className=" grid grid-cols-1 sm:grid-cols-4 p-5 gap-5">
+      {/* <h1 className=" text-center text-6xl"> Sofa are here</h1> */}
+      <div className=" grid grid-cols-1 sm:grid-cols-4 mx-auto gap-5">
         <SingleProduct /> <SingleProduct /> <SingleProduct /> <SingleProduct />{' '}
         <SingleProduct />
       </div>
