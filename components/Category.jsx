b0ef5d93da6733b6e672f5c7b8fa114378e5debc
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-22 21:41:35 +0500

    fixed the styling and fetching data through the backend

--- components/Category.jsx
+++ components/Category.jsx
@@ -2,22 +2,24 @@
 import Image from 'next/image'
 import SOFA from '../public/SOFA.jpg'
 import { useRouter } from 'next/navigation'
-export default function CategoryCard() {
+export default function CategoryCard({ element }) {
   const Router = useRouter()
+
   return (
     //dark:bg-gray-800 dark:border-gray-700
-    <div className="w-full rounded-lg flex flex-col justify-center items-center  bg-white border border-gray-200  hover:shadow-slate-500  shadow-md ">
+    <div className="w-[70vw] sm:w-[30vw] mx-auto rounded-lg flex flex-col justify-center items-center  bg-white border border-gray-200  hover:shadow-slate-500  shadow-md ">
       <Image
-        onClick={() => Router.push('/Products/sofa')}
-        className="rounded-t-lg"
-        src={SOFA}
-        width={400}
-        height={225}
+        onClick={() => Router.push(`/Products/${element.Name}`)}
+        className="rounded-t-lg bg-transparent"
+        src={element.imageUrl}
+        width={150}
+        height={150}
+        alt={element.Name}
       />
 
       <div className=" p-1">
-        <h3 className="text-2xl uppercase font-bold tracking-tight text-slate-700 dark:text-white">
-          Sofa
+        <h3 className="text-2xl uppercase font-bold tracking-tight text-slate-700 dark:text-black">
+          {element.Name}
         </h3>
       </div>
     </div>
--- components/Categorypage.jsx
+++ components/Categorypage.jsx
@@ -1,21 +1,37 @@
-import React from 'react'
-import BlogCard from './ProductCard'
+'use client'
+import React, { useEffect, useState } from 'react'
 import CategoryCard from './Category'
+import { fetchCatogories } from '@/functions/CRUD/Catorgories/Fetch'
+import Loader from './Loader'
 
 const Categorypage = () => {
+  const [Category, SetCategory] = useState([])
+  const [loading, Setloading] = useState(false)
+  useEffect(() => {
+    const GetCatogory = async () => {
+      Setloading(true)
+      const Data = await fetchCatogories('hamza@gmail.com')
+      if (Data) {
+        SetCategory(Data)
+        Setloading(false)
+      }
+    }
+    GetCatogory()
+  }, [])
+  if (loading) return <Loader />
   return (
     <div className=" p-4 ">
       <h1 className=" font-extrabold text-2xl sm:text-4xl text-center sm:text-left sm:px-5">
         Product Categories
       </h1>
-      <div className=" p-4 grid grid-cols-1  justify-center items-center gap-5">
-        <CategoryCard />
-        <CategoryCard />
-        <CategoryCard />
-        <CategoryCard /> <CategoryCard />
-        <CategoryCard />
-        <CategoryCard />
-        <CategoryCard />
+      <div className=" p-4 grid grid-cols-1 sm:grid-cols-3   justify-center items-center gap-5">
+        {Category.map((element, index) => {
+          return (
+            <div key={element.ID}>
+              <CategoryCard element={element} />
+            </div>
+          )
+        })}
       </div>
     </div>
   )
