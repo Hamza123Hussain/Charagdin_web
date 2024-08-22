c6180e188e70595e555da9328a1ae14e2daf0a7b
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-08-23 03:08:54 +0500

    setting data received from the api

--- app/Products/[Name]/page.jsx
+++ app/Products/[Name]/page.jsx
@@ -1,15 +1,38 @@
 'use client'
+import Loader from '@/components/Loader'
 import SingleProduct from '@/components/SingleProduct'
+import { fetchProductsByCategory } from '@/functions/CRUD/Product/GettingProductOfCataGory'
 import * as React from 'react'
+import { useState } from 'react'
 
 export default function MediaCard({ params }) {
+  const [ProductData, SetData] = useState([])
+  const [Loading, SetLoading] = useState(false)
+  console.log(params.Name)
+  React.useEffect(() => {
+    const GetProducts = async () => {
+      SetLoading(true)
+      const Data = await fetchProductsByCategory(params.Name)
+      if (Data) {
+        SetData(Data)
+        SetLoading(false)
+      }
+    }
+    GetProducts()
+  }, [])
+  if (Loading) {
+    return <Loader />
+  }
   return (
     <div className=" p-10 bg-white">
       {' '}
-      {/* <h1 className=" text-center text-6xl"> Sofa are here</h1> */}
+      <h1 className=" text-center text-6xl text-black font-extrabold">
+        {params.Name}s
+      </h1>
       <div className=" grid grid-cols-1 sm:grid-cols-4 mx-auto gap-5">
-        <SingleProduct /> <SingleProduct /> <SingleProduct /> <SingleProduct />{' '}
-        <SingleProduct />
+        {ProductData.map((element, index) => {
+          return <SingleProduct element={element} key={index} />
+        })}
       </div>
     </div>
   )
