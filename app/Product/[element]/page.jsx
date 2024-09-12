2006981e7d849d0df06a4b4482d61598de6f0e4d
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-13 02:52:06 +0500

    fixed issues and componet made

--- app/Product/[element]/page.jsx
+++ app/Product/[element]/page.jsx
@@ -1,29 +1,43 @@
-import React from 'react'
+'use client'
+import React, { useEffect, useState } from 'react'
 import Card from '@mui/material/Card'
+import { fetchProductById } from '@/functions/CRUD/Product/GetSingleProduct'
+import Loader from '@/components/Loader'
 
 const Product = ({ params }) => {
+  const [Data, setData] = useState({})
+  const [loading, setloading] = useState(false)
+  const GetProduct = async () => {
+    setloading(true)
+    const ITEM = await fetchProductById(params.element)
+    if (ITEM) {
+      setData(ITEM)
+      setloading(false)
+    } else {
+      setloading(false)
+    }
+  }
+  useEffect(() => {
+    GetProduct()
+  }, [])
+  if (loading) {
+    return <Loader />
+  }
   return (
-    <div className=" flex ">
-      <Card className="hover:shadow-lg transition-shadow duration-300 rounded-t-xl mx-auto">
-        <img src={params.element.ImageUrl} className=" h-96 w-96" />
+    <div className=" flex bg-white ">
+      <Card className="hover:shadow-lg transition-shadow duration-300 rounded-lg mx-auto my-10">
+        <img src={Data.ImageUrl} className=" h-96 w-96" />
 
         <div className=" my-3 px-2 ">
-          <h6>{params.element.Name}</h6>
+          <h6>{Data.Name}</h6>
         </div>
-        <div className="flex items-center justify-center gap-2 w-full ">
+        <div className="flex items-center justify-center gap-2 w-full  cursor-pointer">
           {' '}
-          <a
-            href="https://wa.me/0314 4058071" // Replace with your WhatsApp number
-            className="fixed bottom-10 right-2  shadow-green-400  bg-green-500 text-white rounded-full flex items-center justify-center shadow-lg z-50 hover:bg-green-600 transition duration-300"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/WhatsApp_icon.png/479px-WhatsApp_icon.png" // Ensure this image is in your public directory
-              alt="WhatsApp"
-              className="w-16 sm:w-12 p-1 "
-            />
-          </a>
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/WhatsApp_icon.png/479px-WhatsApp_icon.png"
+            alt=""
+            width={50}
+          />{' '}
           <h1 className="text-lg font-extrabold ">Call For Price</h1>
         </div>
       </Card>
