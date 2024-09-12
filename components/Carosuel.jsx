138286bb39e540b59b6ac297213dfed7becd64b6
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-13 01:51:56 +0500

    made the images of same size

--- components/Carosuel.jsx
+++ components/Carosuel.jsx
@@ -8,27 +8,23 @@ Swiper.use([Pagination, Autoplay])
 
 const images = [
   {
-    src: 'https://media.licdn.com/dms/image/D4E22AQHdCY49NizGhg/feedshare-shrink_800/0/1692537273395?e=2147483647&v=beta&t=pgGRSnucGMklifrrAoO0dTgmwrXRjOlyUM63u_P8MsQ',
+    src: '/1.jpg',
     text: 'Nature Image 1',
   },
   {
-    src: 'https://media.licdn.com/dms/image/D4D22AQFn4Yj-VcpHtw/feedshare-shrink_800/0/1716219577943?e=2147483647&v=beta&t=vbA66Hkpusa41wE_cFOlp4ueY8YsafihYfxsKxMrxQs',
+    src: '/2.jpg',
     text: 'Nature Image 2',
   },
   {
-    src: 'https://media.licdn.com/dms/image/D4D22AQGIQ1HI_UJ-FA/feedshare-shrink_2048_1536/0/1716219636662?e=2147483647&v=beta&t=QH057TIV9bSx8Qg6ZY-RmcmkMrXSrB-sQbsvoXVhyEA',
+    src: '/3.jpg',
     text: 'Nature Image 3',
   },
   {
-    src: 'https://media.licdn.com/dms/image/D4E22AQETIcxG1RQB7A/feedshare-shrink_800/0/1693752721388?e=2147483647&v=beta&t=ErdW_-RgqPzLkQBvMkinao_2hP0_8K9PDLFhVKUr-rw',
-    text: 'Nature Image 4',
-  },
-  {
-    src: 'https://i.dawn.com/primary/2015/11/563a4ae51fd77.jpg',
+    src: '/5.jpg',
     text: 'Nature Image 5',
   },
   {
-    src: 'https://media.licdn.com/dms/image/D4D22AQGGRnKakZTdzg/feedshare-shrink_800/0/1716219507393?e=2147483647&v=beta&t=MXJkejNbRUDwHevyHhTEAghxZmpV4DBGVf2IARjwJH4',
+    src: '/6.jpg',
     text: 'Nature Image 6',
   },
 ]
@@ -77,7 +73,7 @@ const SwiperCarousel = () => {
                   <img
                     src={image.src}
                     alt={`Slide ${index + 1}`}
-                    className="rounded-t-lg  h-[50vh] w-[100vw] lg:w-[70vw]  sm:h-[60vh] hover:border-black hover:shadow-md border-2 hover:shadow-blue-100  rounded-lg "
+                    className="rounded-t-lg h-96 w-[50vw]  hover:border-black hover:shadow-md border-2 hover:shadow-blue-100  rounded-lg "
                   />
                   <span className="text-3xl mt-5 p-4  text-center font-semibold text-indigo-600">
                     {image.text}
