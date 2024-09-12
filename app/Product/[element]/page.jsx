64a57858825659e0a452b022814ccb0abebcb3b7
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-13 02:55:13 +0500

    added whatsapp number

--- app/Product/[element]/page.jsx
+++ app/Product/[element]/page.jsx
@@ -32,13 +32,19 @@ const Product = ({ params }) => {
           <h6>{Data.Name}</h6>
         </div>
         <div className="flex items-center justify-center gap-2 w-full  cursor-pointer">
-          {' '}
-          <img
-            src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/WhatsApp_icon.png/479px-WhatsApp_icon.png"
-            alt=""
-            width={50}
-          />{' '}
-          <h1 className="text-lg font-extrabold ">Call For Price</h1>
+          <a
+            href="https://wa.me/+92 314 4058071" // Replace with your WhatsApp number
+            className="fixed bottom-10 right-2  shadow-green-400  bg-green-500 text-white rounded-full flex items-center justify-center shadow-lg z-50 hover:bg-green-600 transition duration-300"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/WhatsApp_icon.png/479px-WhatsApp_icon.png"
+              alt=""
+              width={50}
+            />{' '}
+            <h1 className="text-lg font-extrabold ">Call For Price</h1>
+          </a>
         </div>
       </Card>
     </div>
