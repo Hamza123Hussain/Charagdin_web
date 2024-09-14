d697f950817697aeeb7f44702ae4e5ffee20d791
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-14 17:11:53 +0500

    whatsapp icon

--- app/layout.js
+++ app/layout.js
@@ -19,7 +19,7 @@ export default function RootLayout({ children }) {
         <Footer />
         <a
           href="https://wa.me/03144058071" // Replace with your WhatsApp number
-          className="fixed bottom-10 right-2  shadow-green-400  bg-green-500 text-white rounded-full flex items-center justify-center shadow-sm z-10 hover:bg-green-600 transition duration-300"
+          className="fixed bottom-20 right-2  shadow-green-400  bg-green-500 text-white rounded-full flex items-center justify-center shadow-sm z-10 hover:bg-green-600 transition duration-300"
           target="_blank"
           rel="noopener noreferrer"
         >
